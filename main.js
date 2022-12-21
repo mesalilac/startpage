0f4ec4978ff1724d4f66e7912acb71d36236fd82
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-21 20:28:42 +0300

    split creating tabs to main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+at({
+    active: true,
+    id: "Main",
+    links: [
+        al({ lable: "Youtube", href: "https://www.youtube.com/" }),
+        al({ lable: "Wallhaven", href: "https://wallhaven.cc/" }),
+        al({ lable: "gmail", href: "https://mail.google.com/mail/" }),
+        al({ lable: "calendar", href: "https://calendar.google.com/" }),
+    ]
+})
+
+at({
+    id: "Dev",
+    links: [
+        al({ lable: "Github", href: "https://github.com/" }),
+    ]
+})
+
+at({
+    id: "Anime",
+    links: [
+        al({ lable: "Anilist", href: "https://anilist.co/home" }),
+        al({ lable: "MAL", href: "https://myanimelist.net/" }),
+        al({ lable: "Animepahe", href: "https://animepahe.org/" }),
+        al({ lable: "Animixplay", href: "https://animixplay.to/" }),
+        al({ lable: "Subsplease", href: "https://subsplease.org/" }),
+    ]
+})
+
