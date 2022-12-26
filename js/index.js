d9f464d2b3b00c0628b3c13be792c8fd3d72bace
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-26 14:30:25 +0300

    move funcs to lib.js and usage to index.js

--- js/index.js
+++ js/index.js
@@ -1,70 +1,35 @@
-// only functions
+tab({
+    active: true,
+    id: 'Main',
+    links: [
+        link({ lable: 'Youtube', href: 'https://www.youtube.com/' }),
+        link({ lable: 'Wallhaven', href: 'https://wallhaven.cc/' }),
+        link({ lable: 'gmail', href: 'https://mail.google.com/mail/' }),
+        link({ lable: 'calendar', href: 'https://calendar.google.com/' }),
+    ],
+});
 
-/**
-  * @param {Object} opts
-  * @param {string} opts.lable
-  * @param {string} opts.href
-  * @param {string} opts.desc
-*/
-function link(opts) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+tab({
+    id: 'Online shopping',
+    links: [
+        link({ lable: 'Amazon us', href: 'https://www.amazon.com/' }),
+        link({ lable: 'Amazon sa', href: 'https://www.amazon.sa/' }),
+        link({ lable: 'Aliexpress', href: 'https://www.aliexpress.com/' }),
+    ],
+});
 
-    a.text = opts.lable;
-    a.href = opts.href;
-    if (opts.desc) {
-        a.title = opts.desc;
-    }
+tab({
+    id: 'Dev',
+    links: [link({ lable: 'Github', href: 'https://github.com/' })],
+});
 
-    // open in new tab
-    a.target = '_blank';
-
-    li.appendChild(a);
-
-    return li;
-}
-
-/**
-  * @param {Object} opts
-  * @param {string} opts.id
-  * @param {Array} opts.links
-  * @param {boolean} opts.active
-*/
-function tab(opts) {
-    const id = opts.id;
-    const links = opts.links;
-    const active = opts.active || false;
-
-    const tab = document.createElement('button');
-
-    tab.classList.add('tab-button');
-    if (active === true) {
-        tab.classList.add('active');
-    }
-
-    tab.setAttribute('data-id', id);
-    tab.textContent = id;
-
-    const whitespace = document.createTextNode(' ');
-    const buttonsWrapper = document.querySelector('#buttonsWrapper');
-
-    buttonsWrapper.appendChild(tab);
-    buttonsWrapper.appendChild(whitespace);
-
-    const ul = document.createElement('ul');
-
-    ul.classList.add('content');
-    if (active === true) {
-        ul.classList.add('active');
-    }
-
-    ul.id = id;
-
-    for (index in links) {
-        li = links[index];
-
-        ul.appendChild(li);
-    }
-
-    document.querySelector('#contentWrapper').appendChild(ul);
-}
+tab({
+    id: 'Anime',
+    links: [
+        link({ lable: 'Anilist', href: 'https://anilist.co/home' }),
+        link({ lable: 'MAL', href: 'https://myanimelist.net/' }),
+        link({ lable: 'Animepahe', href: 'https://animepahe.org/' }),
+        link({ lable: 'Animixplay', href: 'https://animixplay.to/' }),
+        link({ lable: 'Subsplease', href: 'https://subsplease.org/' }),
+    ],
+});
---
+++ js/lib.js
@@ -0,0 +1,70 @@
+// only functions
+
+/**
+  * @param {Object} opts
+  * @param {string} opts.lable
+  * @param {string} opts.href
+  * @param {string} opts.desc
+*/
+function link(opts) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.text = opts.lable;
+    a.href = opts.href;
+    if (opts.desc) {
+        a.title = opts.desc;
+    }
+
+    // open in new tab
+    a.target = '_blank';
+
+    li.appendChild(a);
+
+    return li;
+}
+
+/**
+  * @param {Object} opts
+  * @param {string} opts.id
+  * @param {Array} opts.links
+  * @param {boolean} opts.active
+*/
+function tab(opts) {
+    const id = opts.id;
+    const links = opts.links;
+    const active = opts.active || false;
+
+    const tab = document.createElement('button');
+
+    tab.classList.add('tab-button');
+    if (active === true) {
+        tab.classList.add('active');
+    }
+
+    tab.setAttribute('data-id', id);
+    tab.textContent = id;
+
+    const whitespace = document.createTextNode(' ');
+    const buttonsWrapper = document.querySelector('#buttonsWrapper');
+
+    buttonsWrapper.appendChild(tab);
+    buttonsWrapper.appendChild(whitespace);
+
+    const ul = document.createElement('ul');
+
+    ul.classList.add('content');
+    if (active === true) {
+        ul.classList.add('active');
+    }
+
+    ul.id = id;
+
+    for (index in links) {
+        li = links[index];
+
+        ul.appendChild(li);
+    }
+
+    document.querySelector('#contentWrapper').appendChild(ul);
+}
