884accd222893383e539ccd76bbf9de44bb33ce2
abdalrzag eisa <bmjfdrh@gmail.com>
2023-05-14 18:46:22 +0300

    change style, and remove some links

--- js/index.js
+++ js/index.js
@@ -2,11 +2,10 @@ new Tab('General', true)
     .add('Youtube', 'https://www.youtube.com')
     .add('Wallhaven', 'https://wallhaven.cc')
     .add('Gmail', 'https://mail.google.com/mail')
-    .add('calendar', 'https://calendar.google.com')
+    .add('Calendar', 'https://calendar.google.com')
     .build();
 
 new Tab('Online shopping')
-    .add('Amazon us', 'https://www.amazon.com')
     .add('Amazon sa', 'https://www.amazon.sa')
     .add('Aliexpress', 'https://www.aliexpress.com')
     .add('Newegg', 'https://www.newegg.com')
@@ -15,13 +14,14 @@ new Tab('Online shopping')
 
 new Tab('Dev')
     .add('Github', 'https://github.com')
+    .add('Stackoverflow', 'https://stackoverflow.com')
     .build();
 
 new Tab('Anime')
     .add('Anilist', 'https://anilist.co/home')
     .add('MAL', 'https://myanimelist.net')
     .add('Animepahe', 'https://animepahe.org')
-    .add('Animixplay', 'https://animixplay.to')
+    .add('Gogoanime', 'https://gogoanime.mom')
     .add('Subsplease', 'https://subsplease.org')
     .build();
 
